309d696dbdb78868f869c7eec41548562c3ad015
HikoKusano16 <mkveton3@gmail.com>
2022-04-28 11:36:32 +0200

    JS

---
+++ 22-28-04/index.js
@@ -0,0 +1,13 @@
+let int = 3;
+
+let str = "Hello World JS";
+
+let isReady = true;
+
+let isDown = false;
+
+console.log(str);
+
+function clickMe(){
+    console.log('The button has been pressed')
+}
