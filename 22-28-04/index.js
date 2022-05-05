9f3cb3300addd19d1b43cff34f1d3cbc24f50473
HikoKusano16 <mkveton3@gmail.com>
2022-05-05 11:29:42 +0200

    My introduction

--- 22-28-04/index.js
+++ 22-28-04/index.js
@@ -1,13 +1,4 @@
-let int = 3;
-
-let str = "Hello World JS";
-
-let isReady = true;
-
-let isDown = false;
-
-console.log(str);
-
-function clickMe(){
-    console.log('The button has been pressed')
-}
+function changeClass() {
+    var element = document.getElementById("showhide");
+    element.classList.toggle("shown");
+  } 
